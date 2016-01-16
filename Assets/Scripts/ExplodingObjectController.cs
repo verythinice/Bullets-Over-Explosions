43b43b8fc1927592bf9d2a5cd0a25b569e29ff04
Roldan Gammad <rgammad@uci.edu>
2016-01-16 13:11:24 -0800

    Pushed ExplosionTrigger and ExplosionObject Controller scripts

--- Assets/Scripts/ExplodingObjectController.cs
+++ Assets/Scripts/ExplodingObjectController.cs
@@ -2,11 +2,16 @@
 using System.Collections;
 
 public class ExplodingObjectController : MonoBehaviour {
-	public Transform prefab;
+	public Transform prefab1;
+	public Transform prefab2;
 
 	void LateUpdate(){
-		if (this.tag == "explode") {
-			Instantiate (prefab, this.transform.position, Quaternion.identity);
+		if (this.tag == "Explode") {
+			
+
+			Instantiate (prefab1, this.transform.position, Quaternion.identity);
+			Instantiate (prefab2, this.transform.position, Quaternion.identity);
+			Destroy (this.gameObject);
 		}
 	}
 }
---
+++ Assets/Scripts/ExplosionTriggerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionTriggerController : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+		//Debug.Log (other.name);
+		if(other.CompareTag("Enemy")){
+			other.tag = "Dead";
+		}
+		if(other.CompareTag("Object")){
+			other.tag = "Explode";
+		}
+		//Destroy (this.gameObject);
+	}
+}
