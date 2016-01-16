3ded022573f1f91c707f97a6d419e72bbe0041bc
verythinice <quake.grizzly@gmail.com>
2016-01-16 13:20:04 -0800

    ExplosionSystems now properly clean up

---
+++ Assets/ParticleSystemKiller.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSystemKiller : MonoBehaviour {
+    private float maxTime;
+    private float time;
+
+	// Use this for initialization
+	void Start () {
+        var systems = GetComponentsInChildren<ParticleSystem>();
+        maxTime = 0;
+        time = 0;
+        foreach (ParticleSystem system in systems)
+        {
+            if (system.startLifetime> maxTime)
+            {
+                maxTime = system.startLifetime;
+            }
+        }
+}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    if (time>= maxTime)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            time += Time.deltaTime;
+        }
+	}
+}
