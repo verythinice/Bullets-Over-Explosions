ae7dbb77751acdc27de41e38ffef25915d1459c1
eaho1 <eaho1@uci.edu>
2016-01-15 20:08:02 -0800

    PlayerController Movement & Mouse Rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,36 +3,40 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
+    public float moveSpeed;
+		
 	void Update () {
 	    if (Input.GetKey(KeyCode.W))
         {
-            //moveUp();
+            this.transform.Translate(new Vector2(0, 1) * moveSpeed * Time.deltaTime, Space.World);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            //moveDown();
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            //moveLeft();
+            this.transform.Translate(new Vector2(0, -1) * moveSpeed * Time.deltaTime, Space.World);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            //moveRight();
+            this.transform.Translate(new Vector2(1, 0) * moveSpeed * Time.deltaTime, Space.World);
         }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(new Vector2(-1, 0) * moveSpeed * Time.deltaTime, Space.World);            
+        }        
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             //shoot();
             Debug.Log("Derp");
         }
+        
+        mouseLook();
+    }    
 
-        Debug.Log(Input.mousePosition);
+    private void mouseLook()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 dir = mousePos - this.transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle - 90, new Vector3(0, 0, 1));
     }
 }
