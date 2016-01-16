94297279bb8b880f23340601dd2489520432174d
verythinice <quake.grizzly@gmail.com>
2016-01-15 19:50:37 -0800

    Started player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.W))
+        {
+            //moveUp();
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //moveDown();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            //moveLeft();
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            //moveRight();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //shoot();
+            Debug.Log("Derp");
+        }
+
+        Debug.Log(Input.mousePosition);
+    }
+}
