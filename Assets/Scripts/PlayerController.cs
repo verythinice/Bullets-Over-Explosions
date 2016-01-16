e585310aee65126455845935c2d0270091ebe93a
eaho1 <eaho1@uci.edu>
2016-01-15 20:27:05 -0800

    Player Shooting Completed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,12 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     public float moveSpeed;
+
+    Vector3 mousePos;
 		
 	void Update () {
+        mouseLook();
+
 	    if (Input.GetKey(KeyCode.W))
         {
             this.transform.Translate(new Vector2(0, 1) * moveSpeed * Time.deltaTime, Space.World);
@@ -25,16 +29,29 @@ public class PlayerController : MonoBehaviour {
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            //shoot();
-            Debug.Log("Derp");
+            Shoot();            
+        }               
+    }
+
+    private void Shoot()
+    {
+        RaycastHit2D[] hitList = Physics2D.RaycastAll(this.transform.position, mousePos - this.transform.position);
+        foreach (RaycastHit2D hit in hitList)
+        {
+            if (hit.collider.CompareTag("Wall"))
+            {
+                break;
+            }
+            if (hit.collider.CompareTag("Enemy"))
+            {
+                Destroy(hit.collider.gameObject);
+            }
         }
-        
-        mouseLook();
-    }    
+    }
 
     private void mouseLook()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 dir = mousePos - this.transform.position;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle - 90, new Vector3(0, 0, 1));
