8f61f03bbd4e59a143222123759fb10d8b4c789a
verythinice <quake.grizzly@gmail.com>
2016-01-15 23:50:07 -0800

    CAMERA SHAKE
    And also the camera controller

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class CameraScript : MonoBehaviour {
+    public float xBuffer = 10f;
+    public float yBuffer = 10f;
+    private Transform player;
 
 	// Use this for initialization
 	void Start () {
-	
+        player = GameObject.Find("Player").transform;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void LateUpdate () {
+        Vector3 playerPosition = player.transform.position;
+        Vector3 targetDirection = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        if (Math.Abs(playerPosition.x - transform.position.x)>xBuffer)
+        {
+            if (playerPosition.x > transform.position.x)
+            {
+                targetDirection.x = playerPosition.x - xBuffer;
+            }
+            else
+            {
+                targetDirection.x = playerPosition.x + xBuffer;
+            }
+        }
+        if (Math.Abs(playerPosition.y - transform.position.y) > yBuffer)
+        {
+            if (playerPosition.y > transform.position.y)
+            {
+                targetDirection.y = playerPosition.y - yBuffer;
+            }
+            else
+            {
+                targetDirection.y = playerPosition.y + yBuffer;
+            }
+        }
+        transform.position = targetDirection;
+    }
+
+    //COME ON AND SLAM
+    //shakeAmount is how big the shake is
+    //shakeTime is how long to shake
+    public void CameraShake(float shakeAmount, float shakeTime)
+    {
+        GetComponentInChildren<CameraShakeScript>().screenShake(shakeAmount, shakeTime);
+    }
 }
---
+++ Assets/Scripts/CameraShakeScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShakeScript : MonoBehaviour
+{
+
+    // How long the object should shake for.
+    public float shake = 0f;
+
+    // Amplitude of the shake. A larger value shakes the camera harder.
+    public float shakeAmount = 0.7f;
+    public float decreaseFactor = 1.0f;
+
+    public Vector3 originalPos;
+
+    private bool shakeStart;
+
+    void Awake()
+    {
+
+    }
+
+    void OnEnable()
+    {
+        originalPos = Camera.main.transform.localPosition;
+    }
+
+    void Update()
+    {
+        //if shakestart then set original and start shaking
+        if (shake > 0 && !shakeStart)
+        {
+            shakeStart = true;
+        }
+        //if shake has started and still shake left
+        else if (shake > 0 && shakeStart)
+        {
+            Camera.main.transform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+            shake -= Time.deltaTime * decreaseFactor;
+        }
+        else if (shake <= 0 && shakeStart)
+        {
+            shake = 0f;
+            Camera.main.transform.localPosition = originalPos;
+        }
+        else
+        {
+            Camera.main.transform.localPosition = originalPos;
+        }
+    }
+
+    //AND WELCOME TO THE JAM
+    public void screenShake(float shakeAmount, float shake)
+    {
+        this.shake = shake;
+        this.shakeAmount = shakeAmount;
+    }
+}
