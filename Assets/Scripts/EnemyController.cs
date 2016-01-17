fe47f3538c51a9f3ef7f95641304408eb2b3337f
eaho1 <eaho1@uci.edu>
2016-01-17 12:20:27 -0800

    Cooler Enemy Controller
    Added specified circle rotation & delayed deaths

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,29 +2,60 @@
 using System.Collections;
 
 public class EnemyController : MonoBehaviour {
+    public enum circleStart { left, right, top, down}
 
 	public float moveSpeed;
 	public float radius;
 	public Transform prefab;
 	public bool circleMove;
+    public circleStart rotateStart = circleStart.right;
 	public GameObject centerPoint;
 
 	float timeCounter = 0;
 
 	void Update(){
-		timeCounter += Time.deltaTime;
+		
 		if (circleMove == true) {
-			float x = Mathf.Cos (timeCounter*moveSpeed);
-			float y = Mathf.Sin (timeCounter*moveSpeed);
-			//float z = 0;
-			transform.position = new Vector2 (centerPoint.transform.position.x + (x * radius), centerPoint.transform.position.y + (y * radius));
+			CircleRotate();
 		}
 	}
 
 	void LateUpdate () {
 		if (this.tag == "Dead") {
-			Instantiate (prefab, this.transform.position, Quaternion.identity);
-            Destroy (this.gameObject);
+            Invoke("Death", .5f);
 		}
 	}
+
+    void CircleRotate()
+    {
+        timeCounter += Time.deltaTime;
+        float x = 0, y = 0;
+        switch (rotateStart)
+        {
+            case circleStart.right:
+                x = Mathf.Cos (timeCounter*moveSpeed);
+		        y = Mathf.Sin (timeCounter*moveSpeed);
+                break;
+            case circleStart.left:
+                x = Mathf.Cos((timeCounter * moveSpeed) + Mathf.PI);
+                y = Mathf.Sin((timeCounter * moveSpeed) + Mathf.PI);
+                break;
+            case circleStart.top:
+                x = Mathf.Cos((timeCounter * moveSpeed) + (Mathf.PI)/2);
+                y = Mathf.Sin((timeCounter * moveSpeed) + (Mathf.PI)/2);
+                break;
+            case circleStart.down:
+                x = Mathf.Cos((timeCounter * moveSpeed) + 3*(Mathf.PI)/2);
+                y = Mathf.Sin((timeCounter * moveSpeed) + 3*(Mathf.PI)/2);
+                break;
+        }
+        	
+		transform.position = new Vector2 (centerPoint.transform.position.x + (x * radius), centerPoint.transform.position.y + (y * radius));
+    }
+
+    void Death()
+    {
+        Instantiate(prefab, this.transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+    }
 }
