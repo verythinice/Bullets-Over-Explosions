0fa7dc3290a80508acb01457145d63a187769bbf
eaho1 <eaho1@uci.edu>
2016-01-16 20:36:44 -0800

    Added Audio Player and changed Player Controller to have ti

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioPlayer : MonoBehaviour {
+
+    public string[] audioClipNames;
+
+    Dictionary<string, AudioClip> library = new Dictionary<string, AudioClip>();
+
+    private static AudioSource source;
+    private float volLowRange = .5f;
+    private float volHighRange = 1.0f;
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        foreach (string name in audioClipNames)
+        {
+            library[name] = Resources.Load("Sounds/" + name) as AudioClip;
+        }
+    }
+
+    public void Play(string clipName)
+    {
+        float vol = Random.Range(volLowRange, volHighRange);
+        source.PlayOneShot(library[clipName], vol);
+    }
+}
