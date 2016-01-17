0519444c073d21b1298529b4ec9acca2d22a9f0e
Roldan Gammad <rgammad@uci.edu>
2016-01-16 16:45:32 -0800

    Added circle movement to enemy controller, added delay to exploding object controller

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,6 +4,21 @@ using System.Collections;
 public class EnemyController : MonoBehaviour {
 
 	public Transform prefab;
+	public float moveSpeed;
+	public bool circleMove;
+	public GameObject centerPoint;
+
+	float timeCounter = 0;
+
+	void Update(){
+		timeCounter += Time.deltaTime;
+		if (circleMove == true) {
+			float x = Mathf.Cos (timeCounter*moveSpeed);
+			float y = Mathf.Sin (timeCounter*moveSpeed);
+			//float z = 0;
+			transform.position = new Vector2 (centerPoint.transform.position.x + x, centerPoint.transform.position.y + y);
+		}
+	}
 
 	void LateUpdate () {
 		if (this.tag == "Dead") {
